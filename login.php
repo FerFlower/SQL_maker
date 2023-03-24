b25272ba3970a263668ba7ce8a7b48b1e6863e25
FerFlower <diegoquispeapaza45@gmail.com>
2023-03-24 15:35:34 -0500

    009

--- login.php
+++ login.php
@@ -1,19 +1,5 @@
 <?php
 echo"Tu servidor no se pudo conectar";
-$servidor = "localhost";
-$usuario="root";
-$contraseña="";
-$archibo="login";
 
-$conexion = mysqli_connect($servidor,$usuario ,$contraseña ,$archibo );
 
-if(isset($_POST['Enviar'])){
-$nombre=$_POST['Nombre'];
-$gmail=$_POST['Gmail'];
-$contra=$_POST['Contraseña'];
-
-$insertarDatos = "INSERT INTO cliente VALUES('$nombre','$gmail','$contra')";
-$sejecutarInsertar = mysqli_query($conexion,$insertarDatos);
-
-}
 ?>
